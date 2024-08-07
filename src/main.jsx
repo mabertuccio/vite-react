e43683b6f60bfea8b9c5ca50ac3b95fdf45307e4
matutuam <tutebertuccio@hotmail.com>
2024-08-07 11:20:39 -0300

    feat: add a button that greets the user

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Welcome from "./components/Welcome.jsx";
+// import Welcome from "./components/Welcome.jsx";
+import Button from "./components/Button.jsx";
 
+/*
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Welcome name="Matias" lastName="Bertuccio" />
+  </React.StrictMode>
+);
+*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Button className="button-red" onClick={() => alert("Hello!")}>
+      Click Here!
+    </Button>
   </React.StrictMode>
 );
